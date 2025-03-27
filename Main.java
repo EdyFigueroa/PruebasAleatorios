cab17efffd806be4609f51e6932b7f0340d658e4
IsaacASC <l23170339@culiacan.tecnm.mx>
2025-03-27 16:00:37 +0000

    Movi los métodos adentro de la calse Main, siguen con nombres incorrectos los scanners

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
 
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String [] args) {
         System.out.print("- Archivo de datos: > ");
         String archivo = sc.nextLine();
