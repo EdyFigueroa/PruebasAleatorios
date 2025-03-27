a8f4a2ace3fa36b03b9f4638084a5806f8dc9387
Edy Jared Figueroa Lizárraga <159922867+EdyFigueroa@users.noreply.github.com>
2025-03-27 16:43:36 +0000

    Menú estandarizado y listo para empezar a usarse

--- Main.java
+++ Main.java
@@ -5,7 +5,11 @@ corresponde a una distribución uniforme (Prueba de Uniformidad). Además de det
  */
 public class Main {
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String [] args) {
+        pantallaDeInicio(); // Imprimir pantalla de inicio
+
+        // Pedir archivo en el que se van a dar los datos
         System.out.print("- Archivo de datos: > ");
         String archivo = sc.nextLine();
         
@@ -22,31 +26,86 @@ public class Main {
             0.8086, 0.8097, 0.8135, 0.8413, 0.8767, 0.8972, 0.9297, 0.9476, 0.9563, 0.9609, 
             0.9862, 0.9909, 0.9928
         };
-        
-        int opcion; // La opción que define la prueba a utilizar
-        do {
+
+        // Comienza lo necesari para el menú
+        int opcion;
+        do { // Ciclo del menú
             System.out.print("- Ingrese su opción: > ");
-            opcion = sc.nextInt();
+            opcion = obtenerIntDel(1, 5);
+
+            // Switch
+            switch (opcion) {
+                case 1:
+                    System.out.println("Bienveids a chi cuadrada"); // Chi por chi
+                    break;
+
+                case 2:
+                    System.out.println("bienvendiso al imalaya xddd");
+                    break;
+
+                case 3:
+                    System.out.println("no me acuerdo ayayay");
+                    break;
+
+                case 4:
+                    System.out.println("hello motto");
+                    break;
+                
+                case 5:
+                    System.out.println("Adiós papure :\"v");
+                    break;
+
+                default:
+                    System.out.println("Ocurrió un error inesperado ayayaya que hiciste");
+                    break;
+            }
         } while (opcion != 5);
+    }
+
+    /**
+     * Imprime una pantalla de inicio con datos de la escuela, grupo, materia, integrantes, etc.
+     */
+    public static void pantallaDeInicio() {
+        // Datos de la escuela
+        System.out.println("=================================================================");
+        System.out.println("              Instituto Tecnológico de Culiacán");
+        System.out.println("               Ing en Sistemas Computacionales");
+
+        // Datos del alumno
+        System.out.println("Pruebas para muestras de números aleatorios");
+        System.out.println("De 12:00 a 13:00 horas");
+
+        // Integrantes del equipo
+        System.out.println("\n> Bojórquez Chávez Yamil Santiago");
+        System.out.println("> Figueroa Lizárraga Edy Jared");
+        System.out.println("> Samano Machado Alexis");
+        System.out.println("> Santiago Contreras Isaac Abdiel");
+
+        // Descripción de la tarea
+        System.out.println("\nTODO");
+        System.out.println("=================================================================");
     }
 
     /**
      * Imprimir las opciones que imprime el menú.
      */
     public static void imprimirMenu() {
-        System.out.println("\t[1] Prueba ji Cuadrada");
+        System.out.println("=================================================================");
+        System.out.println("MENÚ PRINCIPAL");
+        System.out.println("\t[1] Prueba Ji Cuadrada");
         System.out.println("\t[2] Prueba Kolmogorov-Smirnov");
         System.out.println("\t[3] Prueba de las series");
         System.out.println("\t[4] Prueba de las distancias");
-        System.out.println("\t[5] Fin");
+        System.out.println("\t[5] Fin\n");
     }
 
     ///////////// Validaciones
+
     /**
-     * Obtiene un numero en un intervalo cerrado de X a Y; (Donde X y Y son numeros posibles)
-     * @param limiteInferior
-     * @param limiteSuperior
-     * @return
+     * Valida un {@code int} que siempre está dentro del límite especificado.
+     * @param limiteInferior Mínimo de la opción.
+     * @param limiteSuperior Máximo de la opción.
+     * @return Regresa el {@code int} que se obtuvo con la validación.
      */
     public static int obtenerIntDel(int limiteInferior, int limiteSuperior) {
         String stringNumero="a"; int numeroInt=0;
@@ -57,18 +116,18 @@ public class Main {
                 numeroInt = Integer.parseInt(stringNumero);
                 // IF; Es un int, pero es menor o mayor que los limites dados
                 if (numeroInt < limiteInferior || numeroInt > limiteSuperior) {
-                    System.out.print("\t\t [!] Favor de ingresar una opcion valida: ");
+                    System.out.print("- [!] Favor de ingresar una opcion valida: > ");
                 }
             }catch(Exception e) {
-                System.out.print("\t\t [!] Favor de ingresar una opcion valida: ");
+                System.out.print("- [!] Favor de ingresar una opcion valida: > ");
             }
         }while(numeroInt < limiteInferior || numeroInt > limiteSuperior);
         return numeroInt;
     }
 
     /**
-     * Solicitamos un numero Entero cualquiera; Utiliza el metodo esNumero(String)
-     * @return
+     * Valida un {@code int} cualquiera.
+     * @return Regresa un {@code int} validado.
      */
     public static int obtenerInt() {
         String stringNumero="a";
@@ -76,7 +135,7 @@ public class Main {
         do {
             stringNumero = sc.nextLine();
             if (!esNumero(stringNumero)) {
-                System.out.print("\t\t [!] Favor de ingresar una opcion valida: ");
+                System.out.print("- [!] Favor de ingresar una opcion valida: > ");
             }
             
         }while(!esNumero(stringNumero));
@@ -86,8 +145,8 @@ public class Main {
     }
 
     /**
-     * Solicitamos un numero Double cualquiera; Utiliza el metodo esDouble(String)
-     * @return
+     * Valida un {@code double} cualquiera
+     * @return Regresa un {@code double} validado.
      */
     public static double obtenerDouble() {
         String stringNumero="a";
@@ -95,7 +154,7 @@ public class Main {
         do {
             stringNumero = sc.nextLine();
             if (!esDouble(stringNumero)) {
-                System.out.print("\t\t [!] Favor de ingresar una opcion valida: ");
+                System.out.print("- [!] Favor de ingresar una opcion valida: > ");
             }
             
         }while(!esDouble(stringNumero));
@@ -104,9 +163,9 @@ public class Main {
     }
 
     /**
-     * Para checar si una cadena String es un numero valido Int; True si una cadena String es un numero; False si no es un numero
-     * @param enteroEnString
-     * @return
+     * Indica si un {@code String} puede ser utilizado como un {@code int}.
+     * @param enteroEnString El {@code String} que se desea consultar.
+     * @return Un {@code boolean} que dice si el número se puede usar como {@code int}
      */
     public static boolean esNumero(String enteroEnString) {
         // Si es nulo, o esta vacio
@@ -123,9 +182,9 @@ public class Main {
     }
 
     /**
-     * Para checar si una cadena String es un numero valido Double; True si una cadena String es un numero; False si no es un numero
-     * @param doubleEnString
-     * @return
+     * Indica si un {@code String} puede ser utilizado como un {@code double}.
+     * @param enteroEnString El {@code String} que se desea consultar.
+     * @return Un {@code boolean} que dice si el número se puede usar como {@code double}
      */
     public static boolean esDouble(String doubleEnString) {
         // Si es nulo, o esta vacio
